9bc74fe30a1e9401d5e4a2af4a8b89da5cff955b
NguyenLyBaoTran <lytran041206@gmail.com>
2026-04-13 20:24:06 +0700

    Delete SaigonRideProject directory

--- SaigonRideProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SaigonRideProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
