39fadd7950b058d9405cb9140a9024f07f8c19a0
Tran <lytran041206@gmail.com>
2026-05-03 17:26:40 +0700

    merge giao diện + update seed

--- SaigonRideProject/Migrations/20260503102503_fix_amount_wallet.Designer.cs
+++ SaigonRideProject/Migrations/20260503102503_fix_amount_wallet.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SaigonRideProject.Data;
 
@@ -11,9 +12,11 @@ using SaigonRideProject.Data;
 namespace SaigonRideProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260503102503_fix_amount_wallet")]
+    partial class fix_amount_wallet
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
