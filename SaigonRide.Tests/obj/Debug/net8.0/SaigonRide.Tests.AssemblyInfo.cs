0067ee0f507c2c67614fc44a525e7436d8cbc439
Tran <lytran041206@gmail.com>
2026-05-05 15:41:38 +0700

    fix amount + time

--- SaigonRide.Tests/obj/Debug/net8.0/SaigonRide.Tests.AssemblyInfo.cs
+++ SaigonRide.Tests/obj/Debug/net8.0/SaigonRide.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaigonRide.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00776c5542a8df414e93812ab8d26cee69f69ab6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7063740964336cd80c20381725ba6c3e5ee6eb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaigonRide.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaigonRide.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
