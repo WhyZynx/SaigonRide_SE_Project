bf7842e5e19dbb767a4632323a381a3f8fcbe47a
My Ngoc <phungtuan0275@gmail.com>
2026-05-08 08:32:04 +0700

    last report

--- SaigonRide.Tests/obj/Debug/net8.0/SaigonRide.Tests.AssemblyInfo.cs
+++ SaigonRide.Tests/obj/Debug/net8.0/SaigonRide.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaigonRide.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7063740964336cd80c20381725ba6c3e5ee6eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34a4438f7c685a801c621b79b5a1fce72d72d53c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaigonRide.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaigonRide.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
